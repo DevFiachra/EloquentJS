0962f0bb90ddcf6160008f19776d4b5d2a169ae6
Devin Fiachra <devinduffymeade@gmail.com>
2020-01-03 16:41:11 +0100

    problems solved

---
+++ 2_Program_Structure/chessboard.js
@@ -0,0 +1,9 @@
+let ranks = 8;
+
+for (let i = 0; i < ranks; i++) {
+    if (i % 2 === 0) {
+        console.log(" # # # #");
+    } else if (i % 2 === 1) {
+        console.log("# # # # ");
+    }
+}
