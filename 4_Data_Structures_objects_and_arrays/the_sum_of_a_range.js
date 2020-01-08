cebf9cbdc16ce490e82067d500a8baccaf46e3e4
Devin Fiachra <devinduffymeade@gmail.com>
2020-01-08 14:53:06 +0100

    problem solved with bonus solution

--- 4_Data_Structures_objects_and_arrays/the_sum_of_a_range.js
+++ 4_Data_Structures_objects_and_arrays/the_sum_of_a_range.js
@@ -1,8 +1,16 @@
-function range(start, end, step = 1) {
+function range(start, end, step = start < end ? 1 : -1) {
     let array = [];
-    for (let i = start; i <= end; i += step) {
-        array.push(i);
+
+    if (step > 0) {
+        for (let i = start; i <= end; i += step) {
+            array.push(i);
+        }
+    } else {
+        for (let i = start; i >= end; i += step) {
+            array.push(i);
+        }
     }
+
     return array;
 }
 
@@ -16,3 +24,4 @@ function sum(array) {
 
 console.log(range(1, 10));
 console.log(sum(range(1, 10, 2)));
+console.log(sum(range(5, 2, -1)));
